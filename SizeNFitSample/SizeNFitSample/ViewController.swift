da6cae4f61455011192c6beca1572432d8348e3c
“Vidhvaan516” <vidhvaan516@gmail.com>
2020-02-05 14:49:39 +0530

    added SizeNFit Sample with pod file

---
+++ SizeNFitSample/SizeNFitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeNFitSample
+//
+//  Created by Atchu on 05/02/20.
+//  Copyright © 2020 Atchu. All rights reserved.
+//
+
+import UIKit
+import SizeNFit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
